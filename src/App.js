0b0179000f374ef9312a778943a436cc5f41be0d
efulls <saeful.mahmud@gmail.com>
2020-09-07 17:20:10 +0700

    Implement UserInput dan UserOutput

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
 class App extends Component {
+  state = {
+    username:'superfull'
+  }
+
+  usernameChangedHandler = (event) => {
+    this.setState({username:event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ol>
+          <li>Create TWO components: UserInput and UserOutput</li>
+          <li>UserInput should hold an input element, UserOutput two</li>
+          <li>Output multiple UserOutPut compenents in the App component</li>
+          <li>Pass a username (of your choice) to UserOutPut via prop</li>
+          <li>Add state to the App component ( username)</li>
+          <li>Add a methode to manipulate the state ({React.version})</li>
+        </ol>
+  
+        <UserInput 
+          changed={this.usernameChangedHandler} 
+          currentName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName="Yuwana"/>
       </div>
     );
   }
